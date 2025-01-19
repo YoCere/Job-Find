4dd261d28f4ec34c8dad5428ef71d35827d98a7d
YoCere <jcerezorios@gmail.com>
2025-01-19 14:31:10 -0400

    Creating the layout component

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+</head>
+<body>
+<nav>
+            <a href="/">Home</a></li>
+            <a href="/about">About</a></li>
+            <a href="/contact">Contact</a></li>
+    </nav>
+    <h1>About Us</h1>
+    <p>Welcome to our website. Here is some information about us.</p>
+</body>
+</html>
---
+++ resources/views/contact..blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+<nav>
+            <a href="/">Home</a></li>
+            <a href="/about">About</a></li>
+            <a href="/contact">Contact</a></li>
+    </nav>
+    <h1>Contact</h1>
+    <p>Welcome to our website. Here is contact.</p>
+</body>
+</html>
