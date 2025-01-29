8e3f3915c0a28cf294d9bd31394def501c68eecb
YoCere <jcerezorios@gmail.com>
2025-01-29 19:27:43 -0400

    Home page in layout fixed

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
     <h1>Welcome to the About Page</h1>
     <p>This is the about page content.</p>
     <p>Here is some more content.</p>
